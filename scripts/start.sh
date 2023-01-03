e48f2dc8e8ce66b7963dcfebba660f3fb1510422
Abubakar Abid <abubakar@huggingface.co>
2023-01-03 08:59:39 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-docker run --name frps -d --network host -v ~/frp/scripts:/etc/frp frps -c /etc/frp/frps.ini
+docker run --name frps -d --restart unless-stopped --network host -v ~/frp/scripts:/etc/frp frps -c /etc/frp/frps.ini
