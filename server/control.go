1abb88bd85155454d26343f1518df16e3fba2caa
Adrien <adrien@huggingface.co>
2023-02-16 09:02:16 +0100

    Make the server compatible with old process

--- server/control.go
+++ server/control.go
@@ -19,6 +19,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"github.com/fatedier/golib/crypto"
+	"github.com/google/uuid"
 	"io"
 	"net"
 	"runtime/debug"
@@ -461,10 +462,14 @@ func (ctl *Control) manager() {
 				retContent, err := ctl.pluginManager.NewProxy(content)
 				if err == nil {
 					m = &retContent.NewProxy
-					h := sha256.New()
-					h.Write([]byte(m.ProxyName))
-					bs := h.Sum(nil)
-					m.ProxyName = fmt.Sprintf("%x", bs)[:18]
+					if m.ProxyName != "" {
+						h := sha256.New()
+						h.Write([]byte(m.ProxyName))
+						bs := h.Sum(nil)
+						m.ProxyName = fmt.Sprintf("%x", bs)[:18]
+					} else {
+						m.ProxyName = uuid.NewString()[:18]
+					}
 					remoteAddr, err = ctl.RegisterProxy(m)
 				}
 
