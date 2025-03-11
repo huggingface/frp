17c2b2e37975cb55535b0dc9049c0145bee1cc78
aliabd <ali.si3luwa@gmail.com>
2025-03-11 05:12:13 -0700

    add map

--- scripts/connection_logger_server.py
+++ scripts/connection_logger_server.py
@@ -10,6 +10,10 @@ from typing import Optional
 from datetime import datetime
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import requests
+import pycountry
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -302,7 +306,179 @@ def get_ip_address_list() -> list[list[str]]:
     
     return result
 
-with gr.Blocks() as demo:
+IP_API_KEY = os.environ.get("IP_API_KEY")
+
+active_connections = {}
+
+def get_location(ip):
+    response = requests.get(f"https://pro.ip-api.com/json/{ip}?key={IP_API_KEY}")
+    data = response.json()
+    if "bogon" in data:
+        return None
+    if "status" in data and data["status"] == "fail":
+        return None
+
+    return {
+        "ip": ip,
+        "lat": float(data["lat"]),
+        "lon": float(data["lon"]),
+        "country": pycountry.countries.get(alpha_2=data["countryCode"]).alpha_3
+    }
+
+def get_active_ip_addresses(connection_data):  
+    for event in connection_data:
+        event_type, ip, port, run_id, timestamp = event
+        if event_type == "connect":
+            if run_id not in active_connections:
+                active_connections[run_id] = get_location(ip)
+        elif event_type == "disconnect":
+            if run_id in active_connections:
+                del active_connections[run_id]
+
+def create_map_data():
+    countries_counts = {}
+    lons = []
+    lats = []
+    for location in active_connections.values():
+        if location is None:
+            continue
+        if location["country"] not in countries_counts:
+            countries_counts[location["country"]] = 1
+        else:
+            countries_counts[location["country"]] += 1
+        lons.append(location["lon"])
+        lats.append(location["lat"])
+    
+    df = pd.DataFrame({
+        'country': countries_counts.keys(),
+        'ip_count': countries_counts.values()
+    })
+    return df, lons, lats
+
+def create_map():
+    connection_data = get_ip_address_list()
+    get_active_ip_addresses(connection_data)
+    df, lons, lats = create_map_data()
+
+    fig = go.Figure()
+    
+    fig.add_trace(go.Choropleth(
+        locations=df['country'],
+        z=df['ip_count'],
+        text=df['country'] + ': ' + df['ip_count'].astype(str) + ' IPs',
+        colorscale='Reds',
+        showscale=False,
+        marker=dict(
+            line=dict(
+                color='rgb(250, 250, 250)',
+                width=1
+            ),
+            opacity=0.8
+        ),
+        hoverinfo='text+z'
+    ))
+    
+    scatter = go.Scattergeo(
+        lon=lons,
+        lat=lats,
+        mode='markers',
+        marker=dict(
+            size=10,
+            color='rgb(0, 100, 255)',
+            opacity=0.8,
+            symbol='circle',
+            line=dict(
+                width=1,
+                color='rgba(0, 0, 255, 1)'
+            ),
+        ),
+        customdata=["dot-" + str(i) for i in range(len(lons))],
+        hovertemplate="Location %{customdata}<extra></extra>",
+        name='Locations'
+    )
+    
+    fig.add_trace(scatter)
+
+    fig.update_layout(
+        geo=dict(
+            showframe=False,
+            showcoastlines=True,            
+            coastlinecolor='rgb(200, 200, 200)',
+            showland=True,
+            landcolor='rgba(240, 240, 240, 0.3)',
+            showocean=True,
+            oceancolor='rgb(230, 245, 255)',
+            showlakes=True,
+            lakecolor='rgb(230, 245, 255)',
+            showrivers=True,
+            rivercolor='rgb(230, 245, 255)',
+            showcountries=True,
+            countrycolor='rgb(150, 150, 150)',
+            countrywidth=0.5,
+            showsubunits=True,
+            subunitcolor='rgb(180, 180, 180)',
+            subunitwidth=0.35,
+            projection=dict(
+                type='natural earth',
+            ),
+        ),
+        margin=dict(l=0, r=0, t=0, b=0),  
+        # autosize=True,  
+        # height=1000,  
+        # width=1000, 
+        plot_bgcolor='rgb(250, 250, 250)',
+        dragmode=False
+    )
+    
+    return fig
+
+map_demo_css = """
+.fillable {
+    margin: 0;
+    padding: 0;
+    max-width: none;
+    max-height: none;
+    width: 100%;
+}
+.gradio-container {
+    max-width: 100% !important; 
+    width: 100% !important; 
+    height: 100vh !important; 
+    padding: 0 !important;
+}
+.modebar {
+    display: none !important;
+}
+
+@keyframes pulse {
+  0%, 100% {
+    fill: #34a639;
+    fill-opacity: 0;
+    stroke-opacity: 0;
+    stroke-width: 1px;
+    stroke: #34a639;
+    }
+
+    50% {
+        fill: #34a639;
+        fill-opacity: 1;
+        stroke-opacity: 1;
+        stroke-width: 12px;
+        stroke: #34a639;
+    }
+}
+
+/* Apply the animation to each point */
+.point {
+  animation: pulse 1.5s infinite ease-in-out;
+  transform-origin: center;
+  transform-box: fill-box;
+}
+
+
+"""
+
+with gr.Blocks(map_demo_css) as demo:
     with gr.Row():
         with gr.Column():
             minute_bar_plot = gr.BarPlot(x="minute", y="connections", color="metric_type", 
@@ -331,6 +507,12 @@ with gr.Blocks() as demo:
     timer.tick(read_db_and_plot_connections, None, [average_minute, minute_bar_plot, average_hour, hour_bar_plot, duration_plot, num_connections])
 
 with demo.route("IP Addresses") as ip_route:
+    map_plot = gr.Plot(
+            value=create_map,
+            container=False,
+            show_label=False,
+            every=1
+        )
     with gr.Row():
         ip_plot = gr.Dataframe(
             headers=["Event Type", "IP Address", "Port", "RunID", "Timestamp"], 
