25e4699aede8eb43ad7cdbb539d909883b828ec2
aliabd <ali.si3luwa@gmail.com>
2025-03-11 07:08:10 -0700

    fix css

--- scripts/connection_logger_server.py
+++ scripts/connection_logger_server.py
@@ -478,7 +478,7 @@ map_demo_css = """
 
 """
 
-with gr.Blocks(map_demo_css) as demo:
+with gr.Blocks(css=map_demo_css) as demo:
     with gr.Row():
         with gr.Column():
             minute_bar_plot = gr.BarPlot(x="minute", y="connections", color="metric_type", 
