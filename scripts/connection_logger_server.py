03d06d0ce31138ae57075351a61ea3f49625a9de
Abubakar Abid <abubakar@huggingface.co>
2025-03-11 20:47:50 -0700

    changes

--- scripts/connection_logger_server.py
+++ scripts/connection_logger_server.py
@@ -319,7 +319,7 @@ def get_ip_address_list() -> list[list[str]]:
 active_connections = {}
 
 def get_location(ip):
-    response = requests.get(f"https://ip-api.com/json/{ip}")
+    response = requests.get(f"http://ip-api.com/json/{ip}")
     data = response.json()
     if "bogon" in data:
         return None
