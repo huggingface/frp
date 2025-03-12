213f962fcbc4c531c556d49ffa24692a6843d782
Abubakar Abid <abubakar@huggingface.co>
2025-03-11 21:22:04 -0700

    changes

--- scripts/connection_logger_server.py
+++ scripts/connection_logger_server.py
@@ -438,8 +438,7 @@ def create_map():
         ),
         margin=dict(l=0, r=0, t=0, b=0),  
         # autosize=True,  
-        height=1000,  
-        # width=1000, 
+        width=1000, 
         plot_bgcolor='rgb(250, 250, 250)',
         dragmode=False
     )
